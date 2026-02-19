e687b26ed6f46934518d7823db3e1671e979e733
Tony Villegas <luis.villegas@ipejal.gob.mx>
2026-02-19 16:18:37 -0600

    Fix: Append domain suffix to Google Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const CLIENT_ID = `${import.meta.env.VITE_GOOGLE_CLIENT_ID}.apps.googleusercontent.com`;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
